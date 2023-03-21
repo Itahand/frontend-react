246a09e3aaf82cb3daf68666aa4443ecee0a14b9
Itahand Naizir <itahand.naizir@gmail.com>
2023-03-21 18:20:34 -0500

    Reworked minting function

--- src/signTxn/blockChainScripts.js
+++ src/signTxn/blockChainScripts.js
@@ -1,77 +1,46 @@
 /** @format */
 
 require("dotenv").config();
+const mintScript = require("./mintScript");
+const axios = require("axios");
 
-const fcl = require("@onflow/fcl");
-const { authorizationFunction } = require("./authorization");
-
-fcl.config().put("accessNode.api", "https://rest-testnet.onflow.org");
-
-// This function validates the transaction
-const validateTransaction = (transactionDetails) => {
-  if (transactionDetails.status === 4 && transactionDetails.statusCode === 0) {
-    console.log("Successful");
-    return "Successful";
-  } else {
-    return transactionDetails;
-  }
-};
-
-// const utf8ToHex = (utf8: string) => Buffer.from(utf8, 'utf8').toString('hex')
-
-// This is the function that pays for the transaction and fees
-
-const proposer = authorizationFunction;
-const payer = authorizationFunction;
-const authorizations = [authorizationFunction];
-
-const mintNFTScript = () => {
-  return `
-  import NonFungibleToken from 0x631e88ae7f1d7c20
-  import Pieces_3 from 0x1ad3c2a8a0bca093
-  import MetadataViews from 0x631e88ae7f1d7c20
-
-  transaction(_metadataId: UInt64, _recipient: Address) {
-
-    let Administrator: &Pieces_3.Administrator
-
-    prepare(admin: AuthAccount) {
-        // Confirm Admin
-        self.Administrator = admin.borrow<&Pieces_3.Administrator>(from: Pieces_3.AdministratorStoragePath)
-                          ?? panic("This account is not the Administrator.")
-    }
-
-    execute {
-        self.Administrator.mintNFT(metadataId: _metadataId, recipient: _recipient)
-    }
-
-
-  }
-  `;
-};
-
-const mintNFT = async (metadataId, address) => {
-  // Args
-  const args = (arg, t) => [arg(metadataId, t.UInt64), arg(address, t.Address)];
-  return new Promise(async (resolve, reject) => {
-    try {
-      const transactionId = await fcl.mutate({
-        cadence: mintNFTScript(),
-        args,
-        proposer: authorizationFunction,
-        payer: authorizationFunction,
-        authorizations: [authorizationFunction],
-        limit: 999,
-      });
-      const transaction = validateTransaction(
-        await fcl.tx(transactionId).onceSealed()
-      );
-      return transaction;
-    } catch (e) {
-      console.log(e);
-      reject(false);
-    }
+const mintNFT = async (twitterId, tweetText, address) => {
+  let data = JSON.stringify({
+    code: mintScript(),
+    arguments: [
+      {
+        type: "UInt64",
+        value: twitterId,
+      },
+      {
+        type: "String",
+        value: tweetText,
+      },
+      {
+        type: "Address",
+        value: address,
+      },
+    ],
   });
+  const config = {
+    method: "post",
+    url: "https://piece.herokuapp.com/v1/accounts/0x1ad3c2a8a0bca093/transactions",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  const result = await axios(config)
+    .then(function (response: any) {
+      return response.data;
+    })
+    .catch(function (error: any) {
+      console.log("Axios Post request error");
+      console.log(error);
+    });
+
+  return result;
 };
 
 module.exports = {
