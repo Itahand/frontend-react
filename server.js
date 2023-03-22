b7a49a0d26de1b9c6f2befb31882bcf395901c20
HemangVora <hemangvora2@gmail.com>
2023-03-22 15:22:20 +0530

    twitter cards functionality integrated

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "build")))
+app.use('/static', express.static(path.join(__dirname, "build/static")));
+app.use('/manifest.json', express.static(path.join(__dirname, "build", "manifest.json")));
+
+
+
+
 
 app.get("/listing/:id", (req, res) => {
   let listingId = req.params.id
@@ -17,31 +22,30 @@ app.get("/listing/:id", (req, res) => {
     }
 
      data = data
-       .replace(/__TITLE__/g, "Home Page")
-       .replace(/__DESCRIPTION__/g, "Home page description.")
-       .replace(/__IMAGE__/g, process.env.REACT_APP_BACKEND_URL+"piece/previewImage/"+listingId);
+      .replace(/__TITLE__/g, "Piece - Turn Tweets into collectibles")
+      .replace(/__DESCRIPTION__/g, "Collect a piece of any moment. Making digital moments better.")
+      .replace(/__IMAGE__/g, process.env.REACT_APP_BACKEND_URL+"piece/previewImage/"+listingId);
 
     res.send(data)
   });
 });
- 
-
 app.get("*", (req, res) => {
-  const filePath = path.join(__dirname, "./build", "index.html");
-  console.log("routed") 
+
+  const filePath = path.resolve(__dirname, "./build", "index.html");
   fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
       return console.log(err);
     }
 
-     data = data
-       .replace(/__TITLE__/g, "Piece - Turn Tweets into collectibles")
-       .replace(/__DESCRIPTION__/g, "Collect a piece of any moment. Making digital moments better");
+    data = data
+    .replace(/__TITLE__/g, "Piece - Turn Tweets into collectibles")
+    .replace(/__DESCRIPTION__/g, "Collect a piece of any moment. Making digital moments better.")
 
     res.send(data)
   });
 });
  
+app.use(express.static(path.resolve(__dirname, "./build")))
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
